aeb14a1282348c0c6cbd60efdf34f7d1591f115d
jkluio132465 <jkluio132465@gmail.com>
2017-11-23 18:37:38 +0800

    seeding前先清空資料

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -12,8 +12,10 @@ class PostsTableSeeder extends Seeder
     public function run()
     {
         //
+        App\Post::truncate();
         foreach (range(1,20) as $number) 
         {
+
         	App\Post::create([
         		'title' => 'title'.$number,
         		'content' =>'content'.$number,
