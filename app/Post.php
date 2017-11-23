8511c69ab4519d588ce3a251dc4498d54a7f67c4
jkluio132465 <jkluio132465@gmail.com>
2017-11-23 18:26:46 +0800

    資料表關聯

--- app/Post.php
+++ app/Post.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Comment;
 class Post extends Model
 {
     //
@@ -14,4 +14,9 @@ class Post extends Model
     	'content',
     	'is_feature',
     ];
+
+    public function comments()
+    {
+    	return $this ->hasMany(Comment::class);
+    }
 }
